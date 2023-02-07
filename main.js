64ba49a3bd655b8d191a7377c24536941a125e2c
Denis Altukhov <alt-den87@yandex.ru>
2023-02-07 22:13:37 +0300

    await

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ form.onsubmit = async function (e) {
 
   async function getWeather(city) {
     const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-    const response = fetch(url);
+    const response = await fetch(url);
     const data = await response.json();
     console.log(data);
     return data;
